69782f642ceda4c529ee969088c0c9c70b7ad39d
harihitode <harihitode@gmail.com>
2021-09-07 07:29:32 +0900

    add MMU buffer

--- src/ladybird_mmu.sv
+++ src/ladybird_mmu.sv
@@ -18,24 +18,52 @@ module ladybird_mmu
    input logic              nrst
    );
 
-  assign bus.data = (i_ready & (|i_wstrb)) ? i_data : 'z;
-  assign bus.req = i_valid & i_ready;
-  assign bus.addr = i_addr;
-  assign bus.wstrb = i_wstrb;
+  typedef struct packed {
+    logic              valid;
+    logic              we;
+    logic [2:0]        funct;
+    logic [XLEN-1:0]   addr;
+    logic [XLEN-1:0]   data;
+  } request_t;
+
+  request_t            request_buffer;
+  logic                request_done;
+
+  assign i_ready = ~request_buffer.valid;
+  assign bus.data = (bus.req & request_buffer.we) ? request_buffer.data : 'z;
+  assign bus.req = ~request_done & request_buffer.valid;
+  assign bus.addr = request_buffer.addr;
+  assign bus.wstrb = request_buffer.we ? 4'b0001 : 4'b0000;
+
   assign o_valid = bus.data_gnt;
   assign o_data = bus.data;
 
   always_ff @(posedge clk, negedge anrst) begin
     if (~anrst) begin
-      i_ready <= 'b1;
+      request_buffer <= '0;
+      request_done <= '0;
     end else begin
       if (~nrst) begin
-        i_ready <= 'b1;
+        request_buffer <= '0;
+        request_done <= '0;
       end else begin
-        if ((i_ready & i_valid) && (i_wstrb == '0)) begin
-          i_ready <= 'b0;
-        end else if (o_valid && o_ready) begin
-          i_ready <= 'b1;
+        if (request_buffer.valid & bus.gnt) begin
+          request_done <= '1;
+        end else if (o_ready & o_valid) begin
+          request_done <= '0;
+        end
+        if (i_ready & i_valid) begin
+          request_buffer.valid <= i_ready & i_valid;
+          request_buffer.addr <= i_addr;
+          request_buffer.data <= i_data;
+          request_buffer.funct <= 3'b001;
+          if (i_wstrb == '0) begin
+            request_buffer.we <= '0;
+          end else begin
+            request_buffer.we <= '1;
+          end
+        end else if (o_valid & o_ready) begin
+          request_buffer <= '0;
         end
       end
     end
