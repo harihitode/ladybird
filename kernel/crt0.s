d08eb57aef7aceafd4c9af829230be4218407bff
harihitode <harihitode@gmail.com>
2022-07-17 13:35:16 +0900

    add sample kernels

---
+++ kernel/crt0.s
@@ -0,0 +1,10 @@
+// start up routine, and initialization for C
+  .text
+  .align 2
+  .global _zerokara_hajimeru
+  .type	_zerokara_hajimeru, @function
+ _zerokara_hajimeru:
+  li sp, 0x80002000 // set stack pointer
+  jal ra, main      // call main, currently we do not implement BSS clear
+1:
+  ebreak            // ebreak (more formally, we should call _exit system call)
