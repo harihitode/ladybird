0344fb76168ff12706e9313751002409a2ca3065
harihitode <harihitode@gmail.com>
2022-07-20 23:55:20 +0900

    [sim] add csr stub

--- sim/csr.c
+++ sim/csr.c
@@ -6,6 +6,12 @@ void csr_init(csr_t *csr) {
   csr->sim = NULL;
   csr->trap_handler = NULL;
   csr->mepc = 0;
+  csr->hartid = 0;
+  csr->atp = 0;
+  csr->sie = 0;
+  csr->mscratch = 0;
+  csr->mtvec = 0;
+  csr->mie = 0;
   return;
 }
 
@@ -13,6 +19,27 @@ unsigned csr_csrr(csr_t *csr, unsigned addr) {
   switch (addr) {
   case CSR_ADDR_M_EPC:
     return csr->mepc;
+  case CSR_ADDR_M_HARTID:
+    return csr->hartid;
+  case CSR_ADDR_M_STATUS:
+  case CSR_ADDR_S_STATUS:
+    // TODO
+    return 0;
+  case CSR_ADDR_M_EDELEG:
+  case CSR_ADDR_M_IDELEG:
+  case CSR_ADDR_M_PMPCFG0:
+  case CSR_ADDR_M_PMPADDR0:
+    return 0; // not supported
+  case CSR_ADDR_S_ATP:
+    return csr->atp;
+  case CSR_ADDR_S_IE:
+    return csr->sie;
+  case CSR_ADDR_M_SCRATCH:
+    return csr->mscratch;
+  case CSR_ADDR_M_TVEC:
+    return csr->mtvec;
+  case CSR_ADDR_M_IE:
+    return csr->mie;
   default:
     printf("unknown: CSR[R]: addr: %08x @%08x\n", addr, csr->sim->pc);
     return 0;
@@ -23,6 +50,32 @@ void csr_csrw(csr_t *csr, unsigned addr, unsigned value) {
   switch (addr) {
   case CSR_ADDR_M_EPC:
     csr->mepc = value;
+    break;
+  case CSR_ADDR_M_STATUS:
+  case CSR_ADDR_S_STATUS:
+    // TODO
+    break;
+  case CSR_ADDR_M_HARTID:
+    break; // read only
+  case CSR_ADDR_M_EDELEG:
+  case CSR_ADDR_M_IDELEG:
+  case CSR_ADDR_M_PMPCFG0:
+  case CSR_ADDR_M_PMPADDR0:
+    break; // not supported
+  case CSR_ADDR_S_ATP:
+    csr->atp = value;
+    break;
+  case CSR_ADDR_S_IE:
+    csr->sie = value;
+    break;
+  case CSR_ADDR_M_SCRATCH:
+    csr->mscratch = value;
+    break;
+  case CSR_ADDR_M_TVEC:
+    csr->mtvec = value;
+    break;
+  case CSR_ADDR_M_IE:
+    csr->mie = value;
     break;
   default:
     printf("unknown: CSR[W]: addr: %08x value: %08x @%08x\n", addr, value, csr->sim->pc);
--- sim/csr.h
+++ sim/csr.h
@@ -7,6 +7,12 @@ typedef struct csr_t {
   struct sim_t *sim;
   void (*trap_handler)(unsigned, struct sim_t *);
   unsigned mepc;
+  unsigned hartid;
+  unsigned atp; // address translation & protection
+  unsigned sie; // [S] interrupt enable
+  unsigned mscratch;
+  unsigned mtvec; // trap base address
+  unsigned mie; // [M] interrupt enable
 } csr_t;
 
 void csr_init(csr_t *);
@@ -19,5 +25,17 @@ void csr_trap(csr_t *, unsigned trap_code);
 void csr_fini(csr_t *);
 
 #define CSR_ADDR_M_EPC 0x00000341
+#define CSR_ADDR_M_HARTID 0x00000f14
+#define CSR_ADDR_M_STATUS 0x00000300
+#define CSR_ADDR_S_ATP 0x00000180
+#define CSR_ADDR_M_EDELEG 0x00000302
+#define CSR_ADDR_M_IDELEG 0x00000303
+#define CSR_ADDR_S_IE 0x00000104
+#define CSR_ADDR_M_PMPADDR0 0x000003b0
+#define CSR_ADDR_M_PMPCFG0 0x000003a0
+#define CSR_ADDR_M_SCRATCH 0x00000340
+#define CSR_ADDR_M_TVEC 0x00000305
+#define CSR_ADDR_M_IE 0x00000304
+#define CSR_ADDR_S_STATUS 0x00000100
 
 #endif
