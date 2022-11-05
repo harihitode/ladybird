d3ab393683eb3d5406ccf941521c67844fd83fd9
harihitode <harihitode@gmail.com>
2022-11-05 19:18:35 +0900

    update lldb server simulator

--- sim/gdbstub_sys.c
+++ sim/gdbstub_sys.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include "sim.h"
+#include "gdbstub_sys.h"
 
 #define DBG_SIGHUP 0x01
 #define DBG_SIGINT 0x02
@@ -23,13 +23,11 @@
 #define INSTRUCTION_CEBREAK 0x00009002
 
 // global
-static sim_t *sim;
-int sock, client;
 int dbg_main(struct dbg_state *state);
 
-int dbg_sys_getc() {
+int dbg_sys_getc(struct dbg_state *state) {
   char ch;
-  size_t size = recv(client, &ch, 1, MSG_WAITALL);
+  size_t size = recv(state->client, &ch, 1, MSG_WAITALL);
   if (size == 0) {
     return EOF;
   } else {
@@ -37,147 +35,135 @@ int dbg_sys_getc() {
   }
 }
 
-int dbg_sys_putchar(int ch) {
-  send(client, &ch, 1, 0);
+int dbg_sys_putchar(struct dbg_state *state, int ch) {
+  send(state->client, &ch, 1, 0);
   return 0;
 }
 
-int dbg_sys_reg_read(address regno, unsigned *val) {
-  *val = sim_read_register(sim, regno);
+int dbg_sys_reg_read(struct dbg_state *state, address regno, unsigned *val) {
+  *val = sim_read_register(state->sim, regno);
   return 0;
 }
 
-int dbg_sys_reg_write(address regno, unsigned val) {
-  sim_write_register(sim, regno, val);
+int dbg_sys_reg_write(struct dbg_state *state, address regno, unsigned val) {
+  sim_write_register(state->sim, regno, val);
   return 0;
 }
 
-int dbg_sys_mem_readb(address addr, char *val) {
-  *val = (unsigned char)sim_read_memory(sim, addr);
+int dbg_sys_mem_readb(struct dbg_state *state, address addr, char *val) {
+  *val = (unsigned char)sim_read_memory(state->sim, addr);
   return 0;
 }
 
-int dbg_sys_mem_writeb(address addr, char val) {
-  sim_write_memory(sim, addr, val);
+int dbg_sys_mem_writeb(struct dbg_state *state, address addr, char val) {
+  sim_write_memory(state->sim, addr, val);
   return 0;
 }
 
-int dbg_sys_continue() {
-  sim_resume(sim);
+int dbg_sys_continue(struct dbg_state *state) {
+  sim_resume(state->sim);
   return 0;
 }
 
-int dbg_sys_step() {
-  sim_single_step(sim);
+int dbg_sys_step(struct dbg_state *state) {
+  sim_single_step(state->sim);
   return 0;
 }
 
-int dbg_sys_kill() {
+int dbg_sys_kill(struct dbg_state *state) {
+  state->sim->state = quit;
   return 0;
 }
 
-int dbg_sys_set_bw_point(address addr, int type, int kind) {
-  struct dbg_break_watch *pp = NULL;
-  int find = 0;
+int dbg_sys_set_bw_point(struct dbg_state *state, address addr, int type, int kind) {
   if (type < 0 || type > 5) {
-    return 1;
+    return -1;
   }
-  for (struct dbg_break_watch *p = sim->bw; p != NULL; p = p->next) {
-    pp = p;
-    if (p->addr == addr && p->type == type && p->kind == kind) {
-      find = 1;
-      break;
-    }
-  }
-  if (!find) {
-    struct dbg_break_watch *np = (struct dbg_break_watch *)malloc(sizeof(struct dbg_break_watch));
-    np->addr = addr;
-    np->type = type;
-    np->kind = kind;
-    np->value = 0;
-    np->next = NULL;
-    if (pp == NULL) {
-      sim->bw = np;
-    } else {
-      pp->next = np;
-    }
-    switch (type) {
-    case 0: // SW Breakpoint
-      {
-        np->value = np->value | (unsigned char)sim_read_memory(sim, addr);
-        np->value = np->value | ((unsigned char)sim_read_memory(sim, addr + 1) << 8);
-        np->value = np->value | ((unsigned char)sim_read_memory(sim, addr + 2) << 16);
-        np->value = np->value | ((unsigned char)sim_read_memory(sim, addr + 3) << 24);
-        printf("break point to %08x, %08x\n", addr, np->value);
-        if (np->value & 0x03) {
-          // normal
-          sim_write_memory(sim, addr, (char)(0x0ff & INSTRUCTION_EBREAK));
-          sim_write_memory(sim, addr + 1, (char)(0x0ff & (INSTRUCTION_EBREAK >> 8)));
-          sim_write_memory(sim, addr + 2, (char)(0x0ff & (INSTRUCTION_EBREAK >> 16)));
-          sim_write_memory(sim, addr + 3, (char)(0x0ff & (INSTRUCTION_EBREAK >> 24)));
-        } else {
-          // compressed
-          sim_write_memory(sim, addr, (char)(0x0ff & INSTRUCTION_CEBREAK));
-          sim_write_memory(sim, addr + 1, (char)(0x0ff & (INSTRUCTION_CEBREAK >> 8)));
+  switch (type) {
+  case 0: // SW Breakpoint
+    {
+      int new_id = 0;
+      for (int i = 0; i < state->n_bp; i++) {
+        if (state->bp[i].valid && state->bp[i].addr == addr) {
+          return 0;
         }
       }
-      break;
-    case 1: // HW Breakpoint
-      break;
-    case 2: // Watchpoint (write)
-      break;
-    case 3: // Watchpoint (read)
-      break;
-    default: // Watchpoint (access)
-      break;
-    }
-  }
-  return 0;
-}
-
-int dbg_sys_rst_bw_point(address addr, int type, int kind) {
-  struct dbg_break_watch *pp = NULL;
-  for (struct dbg_break_watch *p = sim->bw; p != NULL; p = p->next) {
-    if (p->addr == addr && p->type == type && p->kind == kind) {
-      if (pp == NULL) {
-        sim->bw = NULL;
+      for (new_id = 0; new_id < state->n_bp; new_id++) {
+        if (!state->bp[new_id].valid) break;
+      }
+      unsigned opcode = sim_read_memory(state->sim, addr) & 0x7f;
+      state->bp[new_id].valid = 1;
+      state->bp[new_id].addr = addr;
+      state->bp[new_id].inst = 0;
+      if ((opcode & 0x3) == 0x3) {
+        // normal
+        for (int i = 0; i < 4; i++) {
+          state->bp[new_id].inst |= (unsigned char)sim_read_memory(state->sim, addr + i) << (i * 8);
+          sim_write_memory(state->sim, addr + i, (char)(INSTRUCTION_EBREAK >> (i * 8)));
+        }
       } else {
-        pp->next = p->next;
-      }
-      switch (type) {
-      case 0: // SW Breakpoint
-        printf("break point reset: %08x, %08x\n", addr, p->value);
-        if (p->value & 0x03) {
-          // normal
-          sim_write_memory(sim, addr, (char)(0x0ff & p->value));
-          sim_write_memory(sim, addr + 1, (char)(0x0ff & (p->value >> 8)));
-          sim_write_memory(sim, addr + 2, (char)(0x0ff & (p->value >> 16)));
-          sim_write_memory(sim, addr + 3, (char)(0x0ff & (p->value >> 24)));
-        } else {
-          // compressed
-          sim_write_memory(sim, addr, (char)(0x0ff & p->value));
-          sim_write_memory(sim, addr + 1, (char)(0x0ff & (p->value >> 8)));
+        // compressed
+        for (int i = 0; i < 2; i++) {
+          state->bp[new_id].inst |= (unsigned char)sim_read_memory(state->sim, addr + i) << (i * 8);
+          sim_write_memory(state->sim, addr + i, (char)(INSTRUCTION_CEBREAK >> (i * 8)));
         }
-        break;
-      case 1: // HW Breakpoint
-        break;
-      case 2: // Watchpoint (write)
-        break;
-      case 3: // Watchpoint (read)
-        break;
-      default: // Watchpoint (access)
-        break;
       }
-      free(p);
-      break;
     }
-    pp = p;
+    return 0;
+  case 1: // HW Breakpoint
+    return sim_set_exec_trigger(state->sim, addr, type, kind);
+  case 2: // Watchpoint (write)
+    return sim_set_write_trigger(state->sim, addr, type, kind);
+  case 3: // Watchpoint (read)
+    return sim_set_read_trigger(state->sim, addr, type, kind);
+  default: // Watchpoint (access)
+    return sim_set_access_trigger(state->sim, addr, type, kind);
   }
-  return 0;
 }
 
-char dbg_sys_get_signum() {
+int dbg_sys_rst_bw_point(struct dbg_state *state, address addr, int type, int kind) {
+  if (type < 0 || type > 5) {
+    return -1;
+  }
+  switch (type) {
+  case 0: // SW Breakpoint
+    for (int i = 0; i < state->n_bp; i++) {
+      if (state->bp[i].valid && state->bp[i].addr == addr) {
+        if ((state->bp[i].inst & 0x3) == 0x3) {
+          // normal
+          for (int j = 0; j < 4; j++) {
+            sim_write_memory(state->sim, addr + j, (char)(state->bp[i].inst >> (j * 8)));
+          }
+        } else {
+          // compressed
+          for (int j = 0; j < 2; j++) {
+            sim_write_memory(state->sim, addr + j, (char)(state->bp[i].inst >> (j * 8)));
+          }
+        }
+      }
+    }
+    return 0;
+  case 1: // HW Breakpoint
+    return sim_rst_exec_trigger(state->sim, addr, type, kind);
+  case 2: // Watchpoint (write)
+    return sim_rst_write_trigger(state->sim, addr, type, kind);
+  case 3: // Watchpoint (read)
+    return sim_rst_read_trigger(state->sim, addr, type, kind);
+  default: // Watchpoint (access)
+    return sim_rst_access_trigger(state->sim, addr, type, kind);
+  }
+}
+
+char dbg_sys_get_signum(struct dbg_state *state) {
   return DBG_SIGTRAP;
+}
+
+char *dbg_sys_get_reginfo(struct dbg_state *state, unsigned regno) {
+  return state->sim->reginfo[regno];
+}
+
+char *dbg_sys_get_triple(struct dbg_state *state) {
+  return state->sim->triple;
 }
 
 int main(int argc, char *argv[]) {
@@ -185,26 +171,30 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "%s [ELF FILE] [DISK FILE]\n", argv[0]);
     return 0;
   }
-  sim = (sim_t *)malloc(sizeof(sim_t));
+  struct dbg_state *state = (struct dbg_state *)malloc(sizeof(struct dbg_state));
+  state->sim = (sim_t *)malloc(sizeof(sim_t));
+  state->n_bp = 128;
+  state->bp = (struct breakpoint *)calloc(state->n_bp, sizeof(struct breakpoint));
   char addr[32];
   unsigned short port;
   struct sockaddr_in client_addr;
   unsigned client_len = sizeof(client_addr);
   // initialization
-  sim_init(sim);
-  sim_write_csr(sim, CSR_ADDR_D_CSR, CSR_DCSR_ENABLE_ANY_BREAK);
-  if (sim_load_elf(sim, argv[1]) != 0) {
+  sim_init(state->sim);
+  // set ebreak calling debug callback
+  sim_write_csr(state->sim, CSR_ADDR_D_CSR, CSR_DCSR_EBREAK_M | CSR_DCSR_EBREAK_S | CSR_DCSR_EBREAK_U | PRIVILEGE_MODE_M);
+  if (sim_load_elf(state->sim, argv[1]) != 0) {
     fprintf(stderr, "error in elf file: %s\n", argv[1]);
     goto cleanup;
   }
   if (argc >= 3) {
     // if you open disk file read only mode, set 1 to the last argument below
-    sim_virtio_disk(sim, argv[2], 0);
+    sim_virtio_disk(state->sim, argv[2], 0);
   }
   if (argc >= 5) {
-    sim_uart_io(sim, argv[3], argv[4]);
+    sim_uart_io(state->sim, argv[3], argv[4]);
   } else if (argc >= 4) {
-    sim_uart_io(sim, argv[3], NULL);
+    sim_uart_io(state->sim, argv[3], NULL);
   }
   // config TCP
   port = 12345;
@@ -212,28 +202,30 @@ int main(int argc, char *argv[]) {
   {
     struct sockaddr_in server_addr;
     char yes = 1;
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (const char *)&yes, sizeof(yes));
+    state->sock = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(state->sock, SOL_SOCKET, SO_REUSEADDR, (const char *)&yes, sizeof(yes));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
     inet_pton(AF_INET, addr, &server_addr.sin_addr.s_addr);
-    if (bind(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in)) < 0) {
+    if (bind(state->sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in)) < 0) {
       perror("bind");
       goto cleanup;
     }
-    if (listen(sock, 1) < 0) {
+    if (listen(state->sock, 1) < 0) {
       perror("listen");
       goto cleanup;
     }
   }
   printf("Debug Server Started: %s:%d\n", addr, port);
-  client = accept(sock, (struct sockaddr *)&client_addr, &client_len);
+  state->client = accept(state->sock, (struct sockaddr *)&client_addr, &client_len);
   // start simulation
-  dbg_main(sim);
+  dbg_main(state);
  cleanup:
-  sim_fini(sim);
-  free(sim);
-  close(client);
-  close(sock);
+  sim_fini(state->sim);
+  free(state->sim);
+  free(state->bp);
+  close(state->client);
+  close(state->sock);
+  free(state);
   return 0;
 }
--- sim/gdbstub_sys.h
+++ sim/gdbstub_sys.h
@@ -2,61 +2,23 @@
 #define GDBSTUB_SYS_H
 
 #include "riscv.h"
+#include "sim.h"
 
-struct memory_t;
-struct aclint_t;
-struct csr_t;
-struct elf_t;
-struct core_t;
-
-#define CONFIG_ROM_ADDR 0x00001000
-#define CONFIG_ROM_SIZE 1024
 #define DBG_CPU_NUM_REGISTERS NUM_REGISTERS
 
 typedef unsigned int address;
 
-struct dbg_trigger {
-  unsigned index;
+struct breakpoint {
+  int valid;
   address addr;
-  int type;
-  int kind;
-  unsigned value;
-  struct dbg_trigger *next;
+  address inst;
 };
-
-enum memory_access { MA_NONE, MA_LOAD, MA_STORE };
-
-struct dbg_step_result {
-  unsigned prv;
-  unsigned inst;
-  unsigned rd_regno;
-  unsigned rd_data;
-  unsigned pc_next;
-  unsigned exception_code;
-  enum memory_access mem_access;
-  unsigned mem_virtual_addr;
-  unsigned trapret;
-  unsigned trigger;
-};
-
-enum dbg_state_state { running, quit };
 
 struct dbg_state {
-  enum dbg_state_state state;
-  struct core_t *core;
-  struct memory_t *mem;
-  struct csr_t *csr;
-  struct elf_t *elf;
-  struct dbg_trigger *trigger;
-  struct uart_t *uart;
-  struct disk_t *disk;
-  struct plic_t *plic;
-  struct aclint_t *aclint;
-  unsigned dbg_mode;
-  char **reginfo;  // register information
-  char triple[64]; // triple information
-  char *config_rom;
-  void (*dbg_handler)(struct dbg_state *);
+  sim_t *sim;
+  int sock, client;
+  int n_bp;
+  struct breakpoint *bp;
 };
 
 #endif
