bd584b23a13b38d3c95d6aed3acda848f765141b
harihitode <harihitode@gmail.com>
2023-03-04 02:17:33 +0900

    [sim] update sample launcher

--- sim/launch_sim.c
+++ sim/launch_sim.c
@@ -30,13 +30,34 @@ void hello() {
   fprintf(stderr, "===================\n");
 }
 
-void trigger_set(sim_t *sim) {
-  // set ebreak calling debug callback
-  sim_write_csr(sim, CSR_ADDR_D_CSR, CSR_DCSR_EBREAK_M | CSR_DCSR_EBREAK_S | CSR_DCSR_EBREAK_U | PRIVILEGE_MODE_M);
-  // trigger to memory write to 0x80001000 (tohost)
-  sim_write_csr(sim, CSR_ADDR_T_SELECT, 0);
-  sim_write_csr(sim, CSR_ADDR_T_DATA1, sim_match6(CSR_MATCH6_SELECT_ADDRESS, CSR_MATCH6_TIMING_AFTER, CSR_MATCH6_LOAD | CSR_MATCH6_STORE));
-  sim_write_csr(sim, CSR_ADDR_T_DATA2, TOHOST_ADDR);
+void console_action(sim_t *sim) {
+  unsigned magic_mem = 0;
+  sim_cache_flush(sim);
+  for (int i = 0; i < 4; i++) {
+    magic_mem = magic_mem | ((unsigned char)sim_read_memory(sim, TOHOST_ADDR + i) << (i * 8));
+  }
+  printf("to host: %c, %08x", (char)magic_mem, magic_mem);
+  if (magic_mem & 0x1) {
+    sim->state = quit;
+    fprintf(stderr, "exit with code: %d\n", (int)magic_mem >> 1);
+  } else if (magic_mem != 0) {
+    unsigned which = 0;
+    unsigned arg0 = 0;
+    unsigned arg1 = 0;
+    unsigned arg2 = 0;
+    unsigned test = 0;
+    for (int i = 0; i < 4; i++) {
+      which = which | ((unsigned char)sim_read_memory(sim, magic_mem + i) << (i * 8));
+      arg0 = arg0 | ((unsigned char)sim_read_memory(sim, magic_mem + 8 + i) << (i * 8));
+      arg1 = arg1 | ((unsigned char)sim_read_memory(sim, magic_mem + 16 + i) << (i * 8));
+      arg2 = arg2 | ((unsigned char)sim_read_memory(sim, magic_mem + 24 + i) << (i * 8));
+      test = test | ((unsigned char)sim_read_memory(sim, magic_mem + 32 + i) << (i * 8));
+    }
+    for (unsigned i = 0; i < arg2; i++) {
+      putchar((unsigned char)sim_read_memory(sim, arg1 + i));
+    }
+    sim_write_memory(sim, FROMHOST_ADDR, 0x1);
+  }
 }
 
 void step_handler(struct core_step_result *result) {
@@ -48,37 +69,41 @@ void debug_handler(sim_t *sim) {
   unsigned dcsr = sim_read_csr(sim, CSR_ADDR_D_CSR);
   unsigned cause = (dcsr >> 6) & 0x7;
   if (cause == CSR_DCSR_CAUSE_TRIGGER) {
-    dcsr |= CSR_DCSR_MPRV_EN;
-    sim_write_csr(sim, CSR_ADDR_D_CSR, dcsr);
-    unsigned magic_mem = 0;
-    sim_cache_flush(sim);
-    for (int i = 0; i < 4; i++) {
-      magic_mem = magic_mem | ((unsigned char)sim_read_memory(sim, TOHOST_ADDR + i) << (i * 8));
-    }
-    if (magic_mem & 0x1) {
-      sim->state = quit;
-      fprintf(stderr, "exit with code: %d\n", (int)magic_mem >> 1);
-    } else if (magic_mem != 0) {
-      unsigned which = 0;
-      unsigned arg0 = 0;
-      unsigned arg1 = 0;
-      unsigned arg2 = 0;
-      unsigned test = 0;
-      for (int i = 0; i < 4; i++) {
-        which = which | ((unsigned char)sim_read_memory(sim, magic_mem + i) << (i * 8));
-        arg0 = arg0 | ((unsigned char)sim_read_memory(sim, magic_mem + 8 + i) << (i * 8));
-        arg1 = arg1 | ((unsigned char)sim_read_memory(sim, magic_mem + 16 + i) << (i * 8));
-        arg2 = arg2 | ((unsigned char)sim_read_memory(sim, magic_mem + 24 + i) << (i * 8));
-        test = test | ((unsigned char)sim_read_memory(sim, magic_mem + 32 + i) << (i * 8));
+    int trigger_index = sim_get_trigger_fired(sim);
+    if (trigger_index >= 0) {
+      dcsr |= CSR_DCSR_MPRV_EN;
+      // [M mode] access to MMU -> ON
+      sim_write_csr(sim, CSR_ADDR_D_CSR, dcsr);
+      sim_write_csr(sim, CSR_ADDR_T_SELECT, trigger_index);
+      unsigned tdata1 = sim_read_csr(sim, CSR_ADDR_T_DATA1);
+      switch (sim_get_trigger_type(tdata1)) {
+      case CSR_TDATA1_TYPE_MATCH6: {
+        unsigned addr = sim_read_csr(sim, CSR_ADDR_T_DATA2);
+        if (addr == TOHOST_ADDR) {
+          console_action(sim);
+        } else {
+          unsigned access = tdata1 & 0x07;
+          printf("break at ");
+          if (access == (CSR_MATCH6_STORE | CSR_MATCH6_LOAD)) {
+            printf("access ");
+          } else if (access == CSR_MATCH6_STORE) {
+            printf("store ");
+          } else if (access == CSR_MATCH6_LOAD) {
+            printf("load ");
+          } else {
+            printf("exec ");
+          }
+          printf("%08x\n", addr);
+          sim->state = quit;
+        }
       }
-      for (unsigned i = 0; i < arg2; i++) {
-        putchar((unsigned char)sim_read_memory(sim, arg1 + i));
+      default:
+        break;
       }
-      sim_write_memory(sim, FROMHOST_ADDR, 0x1);
+      // [M mode] access to MMU -> OFF
+      dcsr &= ~CSR_DCSR_MPRV_EN;
+      sim_write_csr(sim, CSR_ADDR_D_CSR, dcsr);
     }
-    trigger_set(sim);
-    dcsr &= ~CSR_DCSR_MPRV_EN;
-    sim_write_csr(sim, CSR_ADDR_D_CSR, dcsr);
   } else if (cause == CSR_DCSR_CAUSE_EBREAK) {
     sim->state = quit;
   }
@@ -93,13 +118,13 @@ int main(int argc, char *argv[]) {
   char log_file_name[128];
   // initialization
   sim_init(sim);
-
-  // [option] for riscv-benchmarks (I/O)
-  // trigger_set(sim);
+  // callback for Debug Extension (for ex. lldb)
+  sim_set_debug_callback(sim, debug_handler);
   // [option] dump cycle instructions (noisy)
   // sim_set_step_callback(sim, step_handler);
-  // [option] callback for Debug Extension (for ex. lldb)
-  // sim_set_debug_callback(sim, debug_handler);
+
+  // [option] set ebreak calling debug callback
+  // sim_write_csr(sim, CSR_ADDR_D_CSR, CSR_DCSR_EBREAK_M | CSR_DCSR_EBREAK_S | CSR_DCSR_EBREAK_U | PRIVILEGE_MODE_M);
 
   // load elf file to ram
   if (sim_load_elf(sim, argv[1]) != 0) {
@@ -124,6 +149,8 @@ int main(int argc, char *argv[]) {
   logfile = fopen(log_file_name, "w");
   fprintf(logfile, "# mnemonic regno R/W after_last_write use_count_by_alu\n");
   // hello();
+  sim_write_register(sim, REG_A0, 0);
+  sim_write_register(sim, REG_A1, DEVTREE_ROM_ADDR);
   while (sim->state == running) {
     sim_resume(sim);
   }
