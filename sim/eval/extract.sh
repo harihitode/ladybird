d30277cf54ac7b28db46b1192c645743db049463
harihitode <harihitode@gmail.com>
2023-03-30 10:51:05 +0900

    [benchmark] add histogram graph scripts

--- sim/eval/extract.sh
+++ sim/eval/extract.sh
@@ -2,6 +2,8 @@
 
 for bench in `cat tests.txt`
 do
+    echo "extract ${bench}"
     awk -e '{ if ($3 == "W") print $0 }' $bench.riscv.log > $bench.log.write
     awk -e '{ if ($3 == "R") print $0 }' $bench.riscv.log > $bench.log.read
+    echo "done"
 done
